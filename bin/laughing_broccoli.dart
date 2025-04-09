db5256d8dc789e347d0e0cd4a0fc0c99765c7d68
JonGunnar04 <jon.gunnar.karlsson@gmail.com>
2025-04-09 18:51:22 +0000

    My first commit

---
+++ bin/laughing_broccoli.dart
@@ -0,0 +1,5 @@
+import 'package:laughing_broccoli/laughing_broccoli.dart' as laughing_broccoli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${laughing_broccoli.calculate()}!');
+}
---
+++ test/laughing_broccoli_test.dart
@@ -0,0 +1,8 @@
+import 'package:laughing_broccoli/laughing_broccoli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
